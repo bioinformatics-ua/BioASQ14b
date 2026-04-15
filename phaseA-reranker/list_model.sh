4b551efc4ffb13a5344d7e5eb45ee91168e4d087
André Ribeiro <andrepedro2004@hotmail.com>
2026-04-15 00:55:06 +0000

    this makes me so happy
    Co-authored-by: Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>

--- phaseA-reranker/list_model.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Define the target directory
-TARGET_DIR="/data/bioasq13/phaseA-reranker/trained_models_b01"
-
-# List only subdirectories
-find "$TARGET_DIR" -mindepth 2 -maxdepth 2 -type d
